6c3c479d9a5e72fa11e01bc81f1f13360a8e5c33
nicolas <n.tiesenhausen@gmail.com>
2019-09-22 17:41:13 +0200

    'deuxieme'

--- app/screens/Login/LoginContainer.js
+++ app/screens/Login/LoginContainer.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component,  } from 'react';
 import LoginView from './LoginView';
 import { connect } from 'react-redux';
+import SplashScreen from 'react-native-splash-screen';
 import * as loginActions from 'app/actions/loginActions';
 
 class LoginContainer extends Component {
     constructor(props) {
         super(props);
+        SplashScreen.hide();
     }
+
 
     render() {
         return <LoginView {...this.props} />;
